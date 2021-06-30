f6adc757d16ee47b526fb85a7160aa2a6624a251
amir <amir15bfk@gmail.com>
2021-06-30 09:05:08 +0100

    AI

---
+++ with AI/agent.py
@@ -0,0 +1,151 @@
+import random
+from typing import final
+import torch
+import numpy as np
+from game import Game
+from collections import deque
+from model import Linear_QNet ,QTrainer
+from ploter import plot
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self) :
+        self.number_of_games = 0
+        self.epsilon = 0 # randomness ??
+        self.gamma = 0.9 # discount rate ??
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft() if is full
+        self.model = Linear_QNet(12,256,4) 
+        self.trainer = QTrainer(self.model,LR,self.gamma) 
+
+    def get_state(self,game):
+        #self.head.x<0 or self.head.y<0 or self.head.x>screen_width or self.head.y>screen_height or self.on_tail(self)
+        # left
+        game.snake.head.x+=-1*game.pixel_size
+        if game.snake.is_lose(game.screen_width,game.screen_height):
+            l_danger=True
+        else:
+            l_danger=False
+        game.snake.head.x+=game.pixel_size
+        
+        # right
+        game.snake.head.x+=game.pixel_size
+        if game.snake.is_lose(game.screen_width,game.screen_height):
+            r_danger=True
+        else:
+            r_danger=False
+        game.snake.head.x+=-1*game.pixel_size
+
+        # up
+        game.snake.head.y+=-1*game.pixel_size
+        if game.snake.is_lose(game.screen_width,game.screen_height):
+            u_danger=True
+        else:
+            u_danger=False
+        game.snake.head.y+=game.pixel_size
+        
+        # down
+        game.snake.head.y+=game.pixel_size
+        if game.snake.is_lose(game.screen_width,game.screen_height):
+            d_danger=True
+        else:
+            d_danger=False
+        game.snake.head.y+=-1*game.pixel_size
+
+        state =[
+            l_danger,
+            r_danger,
+            u_danger,
+            d_danger,
+            game.snake.state[0],
+            game.snake.state[1],
+            game.snake.state[2],
+            game.snake.state[3],
+            game.food.head.x<game.snake.head.x,
+            game.food.head.x>game.snake.head.x,
+            game.food.head.y<game.snake.head.y,
+            game.food.head.y>game.snake.head.y
+        ]
+        return np.array(state,dtype=int)
+
+    def remember(self,state,action,reward,next_state,game_over):
+        self.memory.append((state,action,reward,next_state,game_over))
+
+    def train_long_memory(self):
+        if len(self.memory)>BATCH_SIZE:
+            mini_sample = random.sample(self.memory,BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+        
+        states,actions,rewards,next_states,game_overs = zip(*mini_sample)
+        self.trainer.train_step(states,actions,rewards,next_states,game_overs)
+    
+    def train_short_memory(self,state,action,reward,next_state,game_over):
+        self.trainer.train_step(state,action,reward,next_state,game_over)
+
+    def get_move(self,state):
+        # do a random move
+        self.epsilon = 80 - self.number_of_games
+        final_move = [0,0,0,0]
+
+        if random.randint(0,200) < self.epsilon:
+            move = random.randint(0,3)
+            final_move[move]=1
+        else:
+            state0 = torch.tensor(state,dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move]=1
+        return final_move
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = Game()
+    while True:
+        # get corr state
+        state = agent.get_state(game)
+
+        # get the move
+        action = agent.get_move(state)
+        
+        # play the move
+        reward , game_over , score = game.play_round(action,agent.number_of_games)
+
+        # get the new state
+        new_state = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state,action,reward,new_state,game_over)
+
+        # remember
+        agent.remember(state,action,reward,new_state,game_over)
+
+        if game_over:
+            agent.number_of_games += 1
+            # train long memory
+            agent.train_long_memory()
+
+            if score > record :
+                record = score
+                agent.model.save('model_v4.pth')
+            # TODO : plot
+            plot_scores.append(score)
+            total_score+= score
+            mean_score = total_score/ agent.number_of_games
+
+            print('Game:',agent.number_of_games,'Score:',score,'Record:',record,'Mean:',mean_score)
+            
+            # plot_mean_scores.append(mean_score)
+            # plot(plot_scores,plot_mean_scores)
+
+
+
+
+
+if __name__=='__main__' :
+    train()
---
+++ with AI/game.py
@@ -0,0 +1,167 @@
+import pygame ,sys,random,math
+
+#general setup
+
+
+# snake
+class Snake():
+    def __init__(self,pixel_size):
+        self.snake_color= pygame.Color(0,206,0)
+        self.snake_head_color= pygame.Color(0,100,0)
+        self.size=1
+        self.tail=[]
+        self.state =  [0,0,0,1]
+        self.frame_itiration=0
+        self.head = pygame.Rect(1,1,pixel_size-2,pixel_size-2)
+    def eat(self,food,pixel_size,screen_width):
+        if abs(food.head.x-self.head.x)<5 and abs(food.head.y-self.head.y)<5:
+            self.size+=1
+            print(self.size)
+            food.reset(self,pixel_size,screen_width)
+            self.frame_itiration=0
+            self.tail.append(pygame.Rect(-500,-500,pixel_size-2,pixel_size-2))
+            return 10*self.size
+        else:
+            return 0
+    def move(self,action,pixel_size,screen_width,screen_height,food):
+
+        
+        if self.size>2:
+            i = self.size-2
+            while i>0:
+                self.tail[i].x=self.tail[i-1].x
+                self.tail[i].y=self.tail[i-1].y
+                i-=1
+        if self.size>1:
+            self.tail[0].x=self.head.x
+            self.tail[0].y=self.head.y
+        if self.state==action:
+            reward = 0
+        else:
+            reward = -1
+        #test action
+        if (action[0] and not (self.state[1])) or (action[1] and not (self.state[0]))or (action[2] and not (self.state[3]))or (action[3] and not (self.state[3])):
+            self.state= action
+
+        #move
+        if self.state[0]:
+            #upp
+            self.head.y +=-1*pixel_size
+        elif self.state[1]:
+            #down
+            self.head.y +=1*pixel_size
+        elif self.state[2]:
+            #left
+            self.head.x +=-1*pixel_size
+        elif self.state[3]:
+            #right
+            self.head.x +=1*pixel_size
+        
+         
+        game_over = False
+        score = self.size
+        if  self.is_lose(screen_width,screen_height) or self.frame_itiration>100*math.log(self.size+1):
+            self.lose()
+            reward = -100
+            game_over = True
+        reward += self.eat(food,pixel_size,screen_width)
+        return reward , game_over , score
+    def is_lose(self,screen_width,screen_height):
+        return self.head.x<0 or self.head.y<0 or self.head.x>screen_width or self.head.y>screen_height or self.on_tail(self)
+    def lose(self):
+        self.size = 1
+        self.tail = []
+        self.state =  [0,0,0,1]
+        self.head.x = 1
+        self.head.y = 1
+        self.frame_itiration=0
+    def draw(self,screen):
+        for i in range(self.size-1):
+            x=100+(i+1)*10
+            if x<256:
+                pygame.draw.rect(screen,pygame.Color(0,x,0),self.tail[i])
+            else:
+                pygame.draw.rect(screen,pygame.Color(0,255,0),self.tail[i])
+        pygame.draw.rect(screen,self.snake_head_color,self.head)
+    def on_tail(self,obj):
+        f= False
+
+        for i in self.tail:
+            if abs(i.x - obj.head.x)<5 and abs(i.y - obj.head.y)<5:
+                f= True
+        return f
+
+
+    
+
+
+# food
+class Food:
+    def __init__(self,snake,pixel_size,screen_width):
+        self.food_color= pygame.Color(206,0,0)
+        self.head = pygame.Rect(0,0,pixel_size-2,pixel_size-2)
+        self.reset(snake,pixel_size,screen_width)
+    
+    def reset(self,snake,pixel_size,screen_width):
+        x = random.randint(0, (screen_width-pixel_size )//pixel_size )
+        y = random.randint(0, (screen_width-pixel_size )//pixel_size )
+        self.head.x = x*pixel_size
+        self.head.y = y*pixel_size
+        if (abs(snake.head.x - self.head.x)<5 and abs(snake.head.y - self.head.y)<5) or snake.on_tail(self):
+            self.reset(snake,pixel_size,screen_width)
+# colors
+
+class Game:
+    def __init__(self):  
+        pygame.init()
+        self.clock = pygame.time.Clock()
+
+        self.fast = True
+
+        # Setting up the main window
+        self.screen_width = 1000
+        self.screen_height = 1000
+        self.pixel_size = 50
+
+        self.screen = pygame.display.set_mode((self.screen_width,self.screen_height))
+        pygame.display.set_caption('Snake')
+        self.snake = Snake(self.pixel_size)
+        self.food = Food(self.snake,self.pixel_size,self.screen_width)
+        self.bg_color= pygame.Color(12,12,12)
+    def play_round(self,action,number_of_games):
+        #handling input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    self.fast = not self.fast
+        self.snake.frame_itiration+=1
+        reward ,game_over , score =  self.snake.move(action,self.pixel_size,self.screen_width,self.screen_height,self.food)
+
+        # Draw
+        self.screen.fill(self.bg_color)
+        self.snake.draw(self.screen)
+
+        pygame.draw.ellipse(self.screen,self.food.food_color,self.food.head)
+        # Updating the window
+        pygame.display.flip()
+        if self.fast:
+            self.clock.tick(10000)
+        else:
+            self.clock.tick(30)
+        return reward,game_over , score-1
+
+    
+    
+# game_1 =Game()   
+
+# while True:
+#     action =[0,0,1,0]
+    
+#     if game_1.snake.frame_itiration>10:
+#         action =[0,1,0,0]
+#     reward ,game_over , score = game_1.play_round(action)
+
+    
